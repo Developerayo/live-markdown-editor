332510eb8f5a4726dd709bc74810e4a22e5a90ed
Shodipo Ayomide <shodipovi@gmail.com>
2019-06-19 21:34:06 +0400

    deploy to now

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from "react"
+import React from 'react'
 import ReactDOM from "react-dom"
 import "./index.css"
 import Remarkable from "remarkable"
+class LiveMarkdown extends React.Component
+{
 
-class LiveMarkdown extends React.Component {
-  constructor(props) {
+  constructor (props)
+  {
     super(props)
     this.handleChange = this.handleChange.bind(this)
     this.state = {
@@ -12,40 +14,43 @@ class LiveMarkdown extends React.Component {
     }
   }
 
-  handleChange(e) {
+  handleChange (e)
+  {
     this.setState({
       value: e.target.value,
     })
   }
 
-  getRawMarkup() {
+  getRawMarkup ()
+  {
     const md = new Remarkable()
     return { __html: md.render(this.state.value) }
   }
 
-  render() {
+  render ()
+  {
     return (
-      <div className="container">
-        <div className="input">
-          <h3>Your Markdown here</h3>
-          <textarea
-            className="input-text"
-            onChange={this.handleChange}
-            defaultValue={this.state.value}
-          />
+        <div className="container">
+          <div className="input">
+            <h3>Your Markdown here</h3>
+            <textarea
+              className="input-text"
+              onChange={this.handleChange}
+              defaultValue={this.state.value}
+            />
+          </div>
+          <div className="output">
+            <h3>Output</h3>
+            <div
+              style={{ color: `black` }}
+              dangerouslySetInnerHTML={this.getRawMarkup()}
+              className="output-text"
+            />
+          </div>
         </div>
-        <div className="output">
-          <h3>Output</h3>
-          <div
-            style={{ color: `black` }}
-            dangerouslySetInnerHTML={this.getRawMarkup()}
-            className="output-text"
-          />
-        </div>
-      </div>
-    )
-  }
-}
+    );
+  };
+};
 
 // ========================================
 
